e6665d5ba2e44ecbd3e078b84d7f2308be2d1856
maadmaqboolimaad <134434412+maadmaqboolimaad@users.noreply.github.com>
2023-05-27 13:21:27 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,18 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
-global.location = "Africa, Egypt, Cairo" //ur location
-
-//bot bomdy 
-global.ownernomer = "201098906252" //ur number
+global.owner = ['967780121236'] //ur owner number
+global.ownername = "bot ANOS-MD" //ur owner name
+global.location = "ASIA, YMEN, 52" //ur number
 global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
+global.botname = '❤BOT ANOS-MD' //ur bot name
 global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
 global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
 global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
+global.packname = "⍣االعم انوس ياقلبي" //ur sticker watermark packname
 global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.wm = "❤️ANOS ᗷOT❤️." //ur watermark
 
 // Other
 global.sessionName = 'session'
